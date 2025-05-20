d38d04e293723d68756f243da3a02122285b361a
Jeff Sieu <jeffsy00@gmail.com>
2025-05-20 21:15:39 +0800

    Use h-svh instead of h-dvh to prevent layout shift

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
 					className={clsx(
 						'flex flex-col items-start justify-center',
 						'sm:flex-row sm:items-center',
-						'flex gap-4 h-screen relative',
+						'flex gap-4 h-svh relative',
 						'p-4 sm:p-8',
 					)}
 				>
